58d9c0809a5bc3d82f460ace930030bb3e010af0
Roberto Sosa <betososah@gmail.com>
2013-09-19 18:58:27 -0500

    blah

--- public/real.js
+++ public/real.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 	var $container = $('ul.tweets'),
 	// socket = io.connect('http://localhost:3000/'),
 	socket = io.connect('http://192.241.238.64:3000/'),
-	template = $('#tweetTemplate');          
 	socket.on('twitter', function(data) {
 	console.debug(data);
 		$('.tweets').prepend('<li>' + data.text + '</li>');
